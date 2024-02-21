f6117344415ed961ed097ab5dc8fcd747f50652c
originalmahim <tareqazizmahim@gmail.com>
2024-02-21 12:19:11 +0600

    Connected Mongodb with server successfully

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvou2sg.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
   app.get('/', (req, res) => {
     res.send('Hello Bangladesh!')
   })
